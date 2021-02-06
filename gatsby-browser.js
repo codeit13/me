c192a8b483fb836849ad46b9a22cfaaaa6847be2
Sumit Chauhan <cosmictunnel376@gmail.com>
2021-02-06 22:51:01 +0530

    Added Service Worker Update Functionality

---
+++ gatsby-browser.js
@@ -0,0 +1,9 @@
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `I updated some stuff in my Portfolio. ` +
+      `Would you like to check them out?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
