fa2d255a78f2a5e9057260177ca9808e887a2344
Sumit Chauhan <cosmictunnel376@gmail.com>
2021-02-06 17:01:11 +0530

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,73 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+module.exports = {
+  siteMetadata: {
+    // Used for the title template on pages other than the index site
+    siteTitle: `Sumit Chauhan`,
+    // Default title of the page
+    siteTitleAlt: `Sumit Chauhan`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Dev. Sumit`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://sumit-chauhan.netlify.app`,
+    // Used for SEO
+    siteDescription: `This is a Personal Portfolio for Dev. Sumit Chauhan`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@codeit13`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Sumit Chauhan`,
+        short_name: `Sumit`,
+        description: `Portfolio of Dev. Sumit Chauhan`,
+        start_url: `/`,
+        background_color: `#141821`,
+        theme_color: `#f6ad55`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+}
