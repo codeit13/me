a93ea487cb65e39496798d16bab667aab047e7d7
Sumit Chauhan <cosmictunnel376@gmail.com>
2021-02-09 10:50:16 +0530

    Added Google Search Console Tage

--- src/@lekoarts/gatsby-theme-cara/components/seo.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/seo.tsx
@@ -52,6 +52,7 @@ const SEO = ({ title = ``, description = ``, pathname = ``, image = ``, children
       <link rel="icon" type="image/png" sizes="32x32" href={withPrefix(`/favicon-32x32.png`)} />
       <link rel="icon" type="image/png" sizes="16x16" href={withPrefix(`/favicon-16x16.png`)} />
       <link rel="apple-touch-icon" sizes="180x180" href={withPrefix(`/apple-touch-icon.png`)} />
+      <meta name="google-site-verification" content="tMo53eywpG7-eVTuYj1pdu-Cu9zxHWxk8DRnOoHnugk" />
       <script charset="UTF-8" src="//web.webpushs.com/js/push/a025ba4bcee44bf8f828d6217c2e7110_1.js" async></script>
       {children}
     </Helmet>
